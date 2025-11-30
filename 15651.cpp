d07a4d662a15adde90c4f3f7f71eb40a193fbae5
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2025-11-30 17:21:30 +0900

    Add files via upload

---
+++ 15651.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void solve(int k)
+{
+    if (k == m)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        arr[k] = i;
+        solve(k + 1);
+    }
+}
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    solve(0);
+
+    return 0;
+}
