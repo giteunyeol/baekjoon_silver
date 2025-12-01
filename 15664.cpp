17b42e347d8f7527069a7d46f4a4ca953eeff485
EunYeol <163628847+giteunyeol@users.noreply.github.com>
2025-12-01 18:21:21 +0900

    Add files via upload

---
+++ 15664.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int input_numbers[10];
+int arr[10];
+
+void solve(int cur, int cur_number)
+{
+    if (cur == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    int last = 0;
+    for (int i = cur_number; i < n; i++)
+    {
+        if (last != input_numbers[i])
+        {
+            arr[cur] = input_numbers[i];
+            last = input_numbers[i];
+            solve(cur + 1, i + 1);
+        }
+    }
+}
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input_numbers[i];
+    }
+    sort(input_numbers, input_numbers + n);
+
+    solve(0, 0);
+    return 0;
+}
